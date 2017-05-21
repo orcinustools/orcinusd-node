652fa557d0b8ab4241c195d3f2ae67bbd7b69ebe
anak10thn <anak10thn@gmail.com>
2017-05-21 16:56:23 +0700

    first commit

---
+++ utils.js
@@ -0,0 +1,27 @@
+var process = require('process');
+var yml = require('yamljs');
+var fileExt = require('file-extension');
+var fs = require('fs');
+
+module.exports = {
+  parser : (file)=>{
+    // Favor yaml over json
+    if (fileExt(file) === 'yml') {
+      return yml.load(file);
+    }
+    return require(file);
+  },
+  obj : (val)=>{
+    return Object.keys(val);
+  },
+  getUserHome : ()=>{
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+  },
+  exit : (code) => {
+    code = code || 0;
+    if (global.test) { // Prevent actual exit on unit testing
+      throw new Error();
+    }
+    process.exit(code)
+  }
+}
